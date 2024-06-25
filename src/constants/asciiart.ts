f90af7c1a5ef11f60123e927c6ae976a711342ee
Colin Spence <udappia@gmail.com>
2024-06-25 14:27:11 -0600

    Update asciiart.ts

--- src/constants/asciiart.ts
+++ src/constants/asciiart.ts
@@ -1,33 +1,34 @@
-export const asciiArt = `
-           enclave                                           enclave         
-      ,enclave✺enclave✺                                 .enclave✺enclave✺    
-    ,enclave✺enclave✺encl                              .enclave✺enclave✺enc  
-    enclave✺enclave✺enclave                          enclave✺enclave✺enclav, 
-   enclave✺enclave✺enclave✺en.                    enclave✺enclave✺enclave✺enc
-  ,enclave✺enclave✺enclave✺encl#                enclave✺enclave✺enclave✺encla
-  &enclave✺enclave✺enclave✺enclav.            enclave✺enclave✺enclave✺enclave
-   enclave✺enclave#         enclave          enclave         enclave✺enclave✺
-   #enclave✺encla              encla       (encl.              enclave✺encla 
-   \enclave✺encl               encla       encl                 enclave✺enc/ 
-    /enclave✺enc                 ,enc.    encl                  enclave✺enc  
-      enclave✺en                   enc   &en,                  &enclave✺en   
-        enclave✺@                    en  en                   ,enclave✺@     
-          (enclave✺                   encl                   enclave✺        
-             /enclave                 #en                  enclave           
-                 &enclav               en              /enclav,              
-                      (enclav          @*         *enclav.                   
-                               /&enclave✺enclav%,                            
-                          ,encl&       &        encla                        
-                      encla            en           ,encla                   
-                  #encla,             &en              enclav                
-                enclave              %@ (@               enclave             
-               enclave              %@   ✺@                enclave           
-              enclave✺              en,  enc              enclave✺           
-             enclave✺@            enc,    encl           /enclave✺@          
-            enclave✺encl      .encla       .encla       enclave✺encl         
-            enclave✺enclave✺enclav(          enclave✺enclave✺enclave         
-            enclave✺enclave✺encl(              enclave✺enclave✺encla         
-             enclave✺enclave✺@                    enclave✺enclave✺/          We're hiring
-                enclave✺en                            enclave✺en             https://hiring.enclave.gg
+const output = `�ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P����ɍ���ߞ�C�(jz���m��Q0'�R�I�P�n��r���/P��$�P���
+`
 
-  `
+const createAsciiArt = () => {
+  const splitOutput = output.split('')
+  //   return `
+  //                 eenclaveencla          enclaveencl
+  //                enc         enclave    enc   enclavee
+  //              enc            enclave  e         encla
+  //             enclaveenclaveenclavee            ence
+  //             ence                            encla
+  //             ence                         %enclaveenc
+  //             encla                             enclavee
+  //             enclave               e             enclav
+  //              enclave           en               ence
+  //                enclaveenclaveenc   encla        enc     We're hiring:
+  //                  enclaveenclav      enclaveencee        https://hiring.enclave.gg
+  // `
+  return `
+               ${splitOutput[1]}${splitOutput[2]}${splitOutput[3]}${splitOutput[4]}${splitOutput[5]}${splitOutput[6]}${splitOutput[7]}${splitOutput[8]}${splitOutput[9]}${splitOutput[10]}${splitOutput[11]}${splitOutput[12]}${splitOutput[13]}         ${splitOutput[14]}${splitOutput[15]}${splitOutput[16]}${splitOutput[17]}${splitOutput[18]}${splitOutput[19]}${splitOutput[20]}${splitOutput[21]}${splitOutput[22]}${splitOutput[23]}${splitOutput[24]}            
+             ${splitOutput[25]}${splitOutput[26]}${splitOutput[27]}         ${splitOutput[28]}${splitOutput[29]}${splitOutput[30]}${splitOutput[31]}${splitOutput[32]}${splitOutput[33]}    ${splitOutput[34]}${splitOutput[35]}${splitOutput[36]}   ${splitOutput[37]}${splitOutput[38]}${splitOutput[39]}${splitOutput[40]}${splitOutput[41]}${splitOutput[42]}${splitOutput[43]}${splitOutput[44]}           
+           ${splitOutput[45]}${splitOutput[46]}${splitOutput[47]}            ${splitOutput[48]}${splitOutput[49]}${splitOutput[50]}${splitOutput[51]}${splitOutput[52]}${splitOutput[53]}  ${splitOutput[54]}         ${splitOutput[55]}${splitOutput[56]}${splitOutput[57]}${splitOutput[58]}${splitOutput[59]}           
+          ${splitOutput[60]}${splitOutput[61]}${splitOutput[62]}${splitOutput[63]}${splitOutput[64]}${splitOutput[65]}${splitOutput[66]}${splitOutput[67]}${splitOutput[68]}${splitOutput[69]}${splitOutput[70]}${splitOutput[71]}${splitOutput[72]}${splitOutput[73]}${splitOutput[74]}${splitOutput[75]}${splitOutput[76]}${splitOutput[77]}${splitOutput[78]}${splitOutput[79]}${splitOutput[80]}${splitOutput[81]}            ${splitOutput[82]}${splitOutput[83]}${splitOutput[84]}${splitOutput[85]}            
+          ${splitOutput[86]}${splitOutput[87]}${splitOutput[88]}${splitOutput[89]}                            ${splitOutput[90]}${splitOutput[91]}${splitOutput[92]}${splitOutput[93]}${splitOutput[94]}             
+          ${splitOutput[95]}${splitOutput[96]}${splitOutput[97]}${splitOutput[98]}                         ${splitOutput[99]}${splitOutput[100]}${splitOutput[101]}${splitOutput[102]}${splitOutput[103]}${splitOutput[104]}${splitOutput[105]}${splitOutput[106]}${splitOutput[107]}${splitOutput[108]}${splitOutput[109]}          
+          ${splitOutput[110]}${splitOutput[111]}${splitOutput[112]}${splitOutput[113]}${splitOutput[114]}                             ${splitOutput[115]}${splitOutput[116]}${splitOutput[117]}${splitOutput[118]}${splitOutput[119]}${splitOutput[120]}${splitOutput[121]}         
+          ${splitOutput[122]}${splitOutput[123]}${splitOutput[124]}${splitOutput[125]}${splitOutput[126]}${splitOutput[127]}               ${splitOutput[128]}             ${splitOutput[129]}${splitOutput[130]}${splitOutput[131]}${splitOutput[132]}${splitOutput[133]}${splitOutput[134]}         
+           ${splitOutput[135]}${splitOutput[136]}${splitOutput[137]}${splitOutput[138]}${splitOutput[139]}${splitOutput[140]}${splitOutput[141]}           ${splitOutput[142]}${splitOutput[143]}               ${splitOutput[144]}${splitOutput[145]}${splitOutput[146]}${splitOutput[147]}          
+             ${splitOutput[148]}${splitOutput[149]}${splitOutput[150]}${splitOutput[151]}${splitOutput[152]}${splitOutput[153]}${splitOutput[154]}${splitOutput[155]}${splitOutput[156]}${splitOutput[157]}${splitOutput[158]}${splitOutput[159]}${splitOutput[160]}${splitOutput[161]}${splitOutput[162]}${splitOutput[163]}${splitOutput[164]}   ${splitOutput[165]}${splitOutput[166]}${splitOutput[167]}${splitOutput[168]}${splitOutput[169]}        ${splitOutput[170]}${splitOutput[171]}${splitOutput[172]}     We're hiring:
+               ${splitOutput[173]}${splitOutput[174]}${splitOutput[175]}${splitOutput[176]}${splitOutput[177]}${splitOutput[178]}${splitOutput[179]}${splitOutput[180]}${splitOutput[181]}${splitOutput[182]}${splitOutput[183]}${splitOutput[184]}       ${splitOutput[185]}${splitOutput[186]}${splitOutput[187]}${splitOutput[188]}${splitOutput[189]}${splitOutput[190]}${splitOutput[191]}${splitOutput[192]}${splitOutput[193]}${splitOutput[194]}${splitOutput[195]}${splitOutput[196]}        https://hiring.enclave.gg
+`
+}
+
+export const asciiArt = createAsciiArt()
