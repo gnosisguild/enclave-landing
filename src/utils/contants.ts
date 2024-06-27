0bdd318c33402508b4aec8acfabeb0f261a33edd
juliopavila <javila1997@gmail.com>
2024-06-27 12:37:47 -0300

    feat: improve the code and save the encrypted msg as file

---
+++ src/utils/contants.ts
@@ -0,0 +1,4 @@
+export const HIRING_MSG = `Are you who we're seeking?
+https://hiring.enclave.gg/
+
+There is a difference between knowing the path and walking the path. Should your mind be primed for the ordeal, unravel the ciphertext displayed herein. The required tools are provided. We can only show you the door. You have to walk through it.`
--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -1,13 +1,42 @@
 export const stringToBigUint64Array = (str: string): BigUint64Array => {
+  const encoder = new TextEncoder()
+  const uint8Array = encoder.encode(str)
 
-  const encoder = new TextEncoder();
-  const uint8Array = encoder.encode(str);
+  const buffer = new ArrayBuffer(Math.ceil(uint8Array.length / 8) * 8)
+  const uint8Buffer = new Uint8Array(buffer)
+  uint8Buffer.set(uint8Array)
 
+  return new BigUint64Array(buffer)
+}
 
-  const buffer = new ArrayBuffer(Math.ceil(uint8Array.length / 8) * 8);
-  const uint8Buffer = new Uint8Array(buffer);
-  uint8Buffer.set(uint8Array);
+export const loadBinaryFile = async (filename: RequestInfo | URL) => {
+  const response = await fetch(filename)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${filename}`)
+  }
 
+  if (!response.body) {
+    throw new Error(`Failed to fetch ${filename}`)
+  }
 
-  return new BigUint64Array(buffer);
-};
+  const reader = response?.body?.getReader()
+
+  let receivedLength = 0
+  const chunks = []
+
+  while (true) {
+    const { done, value } = await reader.read()
+    if (done) {
+      break
+    }
+    chunks.push(value)
+    receivedLength += value.length
+  }
+  const chunksAll = new Uint8Array(receivedLength)
+  let position = 0
+  for (let chunk of chunks) {
+    chunksAll.set(chunk, position)
+    position += chunk.length
+  }
+  return chunksAll
+}
