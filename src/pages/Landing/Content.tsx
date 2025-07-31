776ae994fa3619f4523524fbd1e0ca7eeb46f1a3
marv <100447090+eccogrinder@users.noreply.github.com>
2025-07-31 03:01:08 -0500

    Update Content.tsx

--- src/pages/Landing/Content.tsx
+++ src/pages/Landing/Content.tsx
@@ -4,7 +4,9 @@ const Content = () => {
       <img src='/enclave-logo-solid.svg' alt='Enclave Logo' className='w-60' />      
       <div className='flex flex-col gap-4'>
         <p className='text-xl md:text-3xl'>A protocol for verfiable, confidential programs.</p>
-        <p className='text-lg md:text-xl'>Enclave's Encrypted Execution Environments (E3s) make multiparty computation private, provable, and deployable anywhere.</p>
+        <p className='text-lg md:text-xl'>Enclave's Encrypted Execution Environments (E3s) make multiparty computation <br></br>private, provable, and deployable anywhere.</p>
+        <br />
+        <br />
         <a href='https://docs.enclave.gg/' className='text-lg' target='_blank'>
           <strong>Start building on the Devnet &#x2197;</strong>
         </a>
