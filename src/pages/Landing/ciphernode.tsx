a982ee6cf21e3c2d0dd9c7d25c000f0ea071a469
marv <100447090+eccogrinder@users.noreply.github.com>
2025-11-13 18:21:39 -0600

    Refactor Landing component to Ciphernode with iframe

--- src/pages/Landing/ciphernode.tsx
+++ src/pages/Landing/ciphernode.tsx
@@ -1,14 +1,33 @@
-import Content from './Content'
+import React, { useEffect } from 'react'
 
-const Landing = () => {
+const Ciphernode: React.FC = () => {
+  useEffect(() => {
+    // Load the Tally embed script
+    const script = document.createElement('script')
+    script.src = 'https://tally.so/widgets/embed.js'
+    script.async = true
+    document.body.appendChild(script)
+
+    // Clean up on unmount
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
   return (
-    <main className='flex h-full w-full flex-grow flex-col px-6 py-8'>
-      <Content />
-      <div className='pointer-events-none absolute inset-0 z-10 h-full w-full touch-none p-4 sm:inset-auto sm:right-0 sm:top-0 sm:w-auto'>
-        <img src='/icon-stack.svg' alt='Enclave Stack' className='mx-auto h-full opacity-20 sm:opacity-40' />
-      </div>
-    </main>
+    <div className="relative w-full h-screen">
+      <iframe
+        data-tally-src="https://tally.so/r/meJPjo"
+        title="Request Early Access to Run an Enclave Ciphernode"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        className="absolute inset-0 border-0"
+      />
+    </div>
   )
 }
 
-export default Landing
+export default Ciphernode
