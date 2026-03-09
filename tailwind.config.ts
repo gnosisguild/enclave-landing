1adfbd677125c77a1bcd7b94f84cfc1379e8f253
marv <100447090+eccogrinder@users.noreply.github.com>
2026-03-09 18:30:13 -0500

    Expand content paths in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import { colors } from './src/constants/colors.ts'
 
 const config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,mdx}',
+    './pages/**/*.{js,jsx,ts,tsx,mdx}',
+    './*.{js,jsx,ts,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: colors,
