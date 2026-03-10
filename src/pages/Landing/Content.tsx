ad3748f6b4fd53267d1944423d4b7e37aeb56ad9
marv <100447090+eccogrinder@users.noreply.github.com>
2026-03-10 09:45:21 -0500

    Update Content.tsx

--- src/pages/Landing/Content.tsx
+++ src/pages/Landing/Content.tsx
@@ -7,29 +7,19 @@ export default function Content() {
 
   return (
     <>
+      <header className="w-full bg-[#d9fce8]">
+        {/* Announcement bar */}
+        <div className="bg-black px-4 py-2 text-center text-[13px] text-neutral-100 sm:px-6 sm:text-[15px] md:px-10 lg:px-12">
+          Enclave is now The Interfold.{" "}
+          <a
+            href="https://blog.theinterfold.com"
+            className="underline underline-offset-2 transition hover:text-white"
+          >
+            Learn more.
+          </a>
+        </div>
 
-        <header className="w-full bg-[#d9fce8]">
-  <div className="bg-black px-4 py-2 text-center text-[12px] text-neutral-100 sm:px-6 sm:text-sm md:px-10 lg:px-12">
-    Enclave is now The Interfold.{' '}
-    <a
-      href="https://blog.theinterfold.com"
-      className="underline underline-offset-2 transition hover:text-white"
-    >
-      Learn more.
-    </a>
-  </div>
-
-  <div className="mx-auto w-full max-w-5xl px-5 py-2 sm:px-6 md:px-10 lg:px-12">
-    <a href="https://theinterfold.com" className="inline-block">
-      <img
-        src="/interfold-logo.png"
-        alt="The Interfold"
-        className="w-[140px] sm:w-[150px] md:w-[165px] opacity-95 transition duration-500 ease-out hover:-translate-y-[1px] hover:opacity-80 hover:duration-150"
-      />
-    </a>
-  </div>
-</header>
-
+        {/* Logo */}
         <div className="mx-auto w-full max-w-5xl px-5 py-2 sm:px-6 md:px-10 lg:px-12">
           <a href="https://theinterfold.com" className="inline-block">
             <img
