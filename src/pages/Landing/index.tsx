cec6483ea1b4aba08aab5df012ea79ec78c82305
marv <100447090+eccogrinder@users.noreply.github.com>
2026-03-10 00:51:09 -0500

    Update index.tsx

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,12 +2,9 @@ import Content from './Content'
 
 const Landing = () => {
   return (
-    <main className='flex h-full w-full flex-grow flex-col px-6 py-8'>
+    <div className='relative flex h-full w-full flex-grow flex-col'>
       <Content />
-      <div className='pointer-events-none absolute inset-0 z-10 h-full w-full touch-none p-4 sm:inset-auto sm:right-0 sm:top-0 sm:w-auto'>
-        <img src='/icon-stack.svg' alt='Enclave Stack' className='mx-auto h-full opacity-20 sm:opacity-40' />
-      </div>
-    </main>
+    </div>
   )
 }
 
